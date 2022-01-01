90f86368a058e34ae24c7183f8999f7e57233838
eggbread <gyeongmin251@gmail.com>
2022-01-01 21:59:07 +0900

    eggbread 2022_01_01

--- eggbread/solution.py
+++ eggbread/solution.py
@@ -1,10 +1,34 @@
+import time
 """
-Problem : Programmers 'Sample'
-Time : n*logn
-Solution : DFS
+Problem : Programmers 문자열 압축
+Time : n^2
+Solution : Searching
 """
-def solution():
-    return 0
+def solution_20220101(s):
+    minValue = len(s)
+    for i in range(1, int(len(s)/2)+1):
+        prev = s[:i]
+        prev_head = 0
+        prev_tail = i
+        stack = 1
+        n = len(s)
+        for j in range(i,n,i):
+            if prev == s[j:j+i]:
+                stack += 1
+            else:
+                prev = s[j:j+i]
+                prev_head = j
+                prev_tail = j+i
+                if stack > 1:
+                    n -= i*(stack-1) - len(str(stack))
+                    stack = 1
+        if stack >1:
+            n -= i * (stack - 1) - len(str(stack))
+        if minValue > n:
+            minValue = n
+    return minValue
 
 if __name__ == "__main__":
-    solution()
+    test = ["aabbaccc", "ababcdcdababcdcd", "abcabcdede", "abcabcabcabcdededededede", "xababcdcdababcdcd"]
+    for t in test:
+        print(solution(t))
