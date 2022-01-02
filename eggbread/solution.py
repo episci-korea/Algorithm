5d2704b64bc4c9c3627aa71873ed88e6f04e837c
eggbread <gyeongmin251@gmail.com>
2022-01-02 23:49:01 +0900

    2022-01-02

--- eggbread/solution.py
+++ eggbread/solution.py
@@ -1,6 +1,6 @@
 import time
 """
-Problem : Programmers 문자열 압축
+Problem : Programmers 문자열 압축 lv2
 Time : n^2
 Solution : Searching
 """
@@ -28,7 +28,24 @@ def solution_20220101(s):
             minValue = n
     return minValue
 
+"""
+Problem : Programmers 멀쩡한 사각형 lv2
+Time : n
+Solution : greatest common divisor
+"""
+def solution_20220102(w,h):
+    g = gcd(w, h)
+    return w*h - w-h+g
+
+def gcd(a, b):
+    if a < b:
+        (a, b) = (b, a)
+    while b != 0:
+        (a, b) = (b, a % b)
+    return a
+
+
 if __name__ == "__main__":
-    test = ["aabbaccc", "ababcdcdababcdcd", "abcabcdede", "abcabcabcabcdededededede", "xababcdcdababcdcd"]
+    test = []
     for t in test:
         print(solution(t))
