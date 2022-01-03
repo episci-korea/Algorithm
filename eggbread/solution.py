63130544f66a54521e66c8b63d48f694cdd95f53
eggbread <gyeongmin251@gmail.com>
2022-01-03 23:09:43 +0900

    2022-01-03

--- eggbread/solution.py
+++ eggbread/solution.py
@@ -44,8 +44,47 @@ def gcd(a, b):
         (a, b) = (b, a % b)
     return a
 
+"""
+Problem : Programmers 거리두기 확인하기 lv2
+Time : n^2
+Solution : BFS
+"""
+from collections import deque
+def bfs(p):
+    start = []
+    for i in range(5):
+        for j in range(5):
+            if p[i][j] == 'P':
+                start.append([i, j])
+
+    for s in start:
+        queue = deque([s])
+        visited = [[0] * 5 for i in range(5)]
+        distance = [[0] * 5 for i in range(5)]
+        visited[s[0]][s[1]] = 1
+
+        while queue:
+            y, x = queue.popleft()
+            dx = [-1, 1, 0, 0]
+            dy = [0, 0, -1, 1]
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if 0 <= nx < 5 and 0 <= ny < 5 and visited[ny][nx] == 0:
+                    if p[ny][nx] == 'O':
+                        queue.append([ny, nx])
+                        visited[ny][nx] = 1
+                        distance[ny][nx] = distance[y][x] + 1
+                    if p[ny][nx] == 'P' and distance[y][x] <= 1:
+                        return 0
+    return 1
+
+def solution(places):
+    answer = []
+    for i in places:
+        answer.append(bfs(i))
+    return answer
 
 if __name__ == "__main__":
-    test = []
-    for t in test:
-        print(solution(t))
+    test = [["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]
+    print(solution(test))
