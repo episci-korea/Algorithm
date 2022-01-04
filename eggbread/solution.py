2f5400cdf61bac9d68fe0921bb68ed6f2d97d54b
eggbread <gyeongmin251@gmail.com>
2022-01-04 23:10:41 +0900

    2022-01-04

--- eggbread/solution.py
+++ eggbread/solution.py
@@ -79,12 +79,44 @@ def bfs(p):
                         return 0
     return 1
 
-def solution(places):
+def solution_20220103(places):
     answer = []
     for i in places:
         answer.append(bfs(i))
     return answer
 
+"""
+Problem : Programmers 프린터 lv2
+Time : n
+Solution : Queue
+"""
+def solution_20220104(priorities, location):
+    """
+    프로그래머스 : 프린터
+    Θ(n)
+    Queue 사용 문제
+    """
+    answer = 0
+    while True:
+        if len(priorities) < 2:
+            answer += 1
+            break
+        else:
+            value = priorities[0]
+            priorities.pop(0)
+            if value >= max(priorities):
+                answer += 1
+                if location:
+                    location -= 1
+                else:
+                    break
+            else:
+                if location:
+                    location -= 1
+                else:
+                    location = len(priorities)
+                priorities.append(value)
+    return answer
 if __name__ == "__main__":
     test = [["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]
     print(solution(test))
