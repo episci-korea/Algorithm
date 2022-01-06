981b98be5ef18923ffe812f80d52cf9442c074bd
tegg89 <tegg89@gmail.com>
2022-01-06 22:54:15 +0900

    Change prob number

--- tegg/leetcode/1.py
+++ tegg/leetcode/1.py
@@ -1,5 +1,5 @@
 """
-2. Two sum
+1. Two sum
 """
 
 class Solution:
@@ -28,6 +28,6 @@ class Solution:
 
 
 sol = Solution()
-print(sol.twoSum([2, 7, 11, 15], 9))
-print(sol.twoSum([3, 2, 4], 6))
-print(sol.twoSum([3, 3], 6))
+assert sol.twoSum([2, 7, 11, 15], 9) == [0, 1]
+assert sol.twoSum([3, 2, 4], 6) == [1, 2]
+assert sol.twoSum([3, 3], 6) == [0, 1]
